07f0e9347978d7bd8d2aa0d33d91a704a688fbbf
eomelyashko <32617320+cRecKer47@users.noreply.github.com>
2019-10-18 18:58:33 +0400

    lesson04 HW04 replace deprecated method/print error message when fill storage

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -8,6 +8,7 @@ import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 import static org.junit.Assert.*;
+import static ru.javawebinar.basejava.storage.AbstractArrayStorage.STORAGE_LIMIT;
 
 public abstract class AbstractArrayStorageTest {
     private Storage storage;
@@ -17,10 +18,10 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
 
-    private static final Resume resume_uuid0 = new Resume(UUID_0);
-    private static final Resume resume_uuid1 = new Resume(UUID_1);
-    private static final Resume resume_uuid2 = new Resume(UUID_2);
-    private static final Resume resume_uuid3 = new Resume(UUID_3);
+    private static final Resume resume0 = new Resume(UUID_0);
+    private static final Resume resume1 = new Resume(UUID_1);
+    private static final Resume resume2 = new Resume(UUID_2);
+    private static final Resume resume3 = new Resume(UUID_3);
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -29,9 +30,9 @@ public abstract class AbstractArrayStorageTest {
     @Before
     public void setUp() throws Exception {
         storage.clear();
-        storage.save(resume_uuid1);
-        storage.save(resume_uuid2);
-        storage.save(resume_uuid3);
+        storage.save(resume0);
+        storage.save(resume1);
+        storage.save(resume2);
     }
 
     @Test
@@ -42,36 +43,36 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = NotExistStorageException.class)
     public void updateNotExist() {
-        storage.update(resume_uuid0);
+        storage.update(resume3);
     }
 
     @Test
     public void savePassed() {
-        storage.save(resume_uuid0);
+        storage.save(resume3);
         assertEquals(4, storage.size());
     }
 
     @Test(expected = ExistStorageException.class)
     public void saveExist() {
-        storage.save(resume_uuid1);
+        storage.save(resume1);
     }
 
     @Test(expected = StorageException.class)
     public void saveStackOver() {
         try {
-            storage.getAll();
-            for (int i = 4; i <= 10_000; i++) {
+            System.out.println(storage.size());
+            for (int i = storage.size(); i < STORAGE_LIMIT; i++) {
                 storage.save(new Resume("uuid" + i));
             }
         } catch (StorageException e) {
-            fail();
+            fail("Error when fill");
         }
-        storage.save(new Resume("uuid10001"));
+        storage.save(new Resume("uuid10000"));
     }
 
     @Test
     public void getPassed() {
-        assertEquals(resume_uuid3, storage.get(UUID_3));
+        assertEquals(resume0, storage.get(UUID_0));
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -81,13 +82,13 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void deletePassed() {
-        storage.delete(UUID_3);
+        storage.delete(UUID_2);
         assertEquals(2, storage.size());
     }
 
     @Test(expected = NotExistStorageException.class)
     public void deleteNotExist() {
-        storage.delete(UUID_0);
+        storage.delete(UUID_3);
     }
 
     @Test
@@ -97,7 +98,7 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void getAll() {
-        Resume[] resumes = new Resume[] {resume_uuid1, resume_uuid2, resume_uuid3};
-        assertEquals(resumes, storage.getAll());
+        Resume[] resumes = new Resume[] {resume0, resume1, resume2};
+        assertArrayEquals(resumes, storage.getAll());
     }
 }
