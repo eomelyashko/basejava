63b19fd5e958121c0562e3d155b2c75e3ec14ed3
eomelyashko <32617320+cRecKer47@users.noreply.github.com>
2019-10-26 17:18:52 +0400

    lesson05 Static initializer block and Collections

---
+++ src/ru/javawebinar/basejava/MainCollections.java
@@ -0,0 +1,57 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.*;
+
+public class MainCollections {
+
+    private static final String UUID_0 = "uuid0";
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    private static final Resume RESUME_0 = new Resume(UUID_0);
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+
+    public static void main(String[] args) {
+        Collection<Resume> collection = new ArrayList<>();
+        collection.add(RESUME_0);
+        collection.add(RESUME_2);
+        collection.add(RESUME_3);
+
+        for (Resume resume : collection) {
+            System.out.println(resume);
+            // ConcurrentModificationException
+            /*if (Objects.equals(resume.getUuid(), UUID_0)) {
+                collection.remove(resume);
+            }*/
+        }
+
+        Iterator<Resume> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            Resume resume = iterator.next();
+            System.out.println(resume);
+            if (Objects.equals(resume.getUuid(), UUID_0)) {
+                iterator.remove();
+            }
+        }
+        System.out.println(collection.toString());
+
+        Map<String, Resume> map = new HashMap<String, Resume>();
+        map.put(UUID_0, RESUME_0);
+        map.put(UUID_1, RESUME_1);
+        map.put(UUID_2, RESUME_2);
+
+        // Bad!
+        for (String uuid : map.keySet()) {
+            System.out.println(map.get(uuid));
+        }
+
+        for (Map.Entry<String, Resume> entry : map.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+    }
+}
--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -18,10 +18,17 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
 
-    private static final Resume RESUME_0 = new Resume(UUID_0);
-    private static final Resume RESUME_1 = new Resume(UUID_1);
-    private static final Resume RESUME_2 = new Resume(UUID_2);
-    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_0;
+    private static final Resume RESUME_1;
+    private static final Resume RESUME_2;
+    private static final Resume RESUME_3;
+
+    static {
+        RESUME_0 = new Resume(UUID_0);
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+    }
 
     protected AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
